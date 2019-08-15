fe374ec95793b519c29ce9a6dc59d06687f63827
Roman Onatskiy <ronatskiy@users.noreply.github.com>
2019-08-15 23:11:51 +0300

    basic setup

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,12 @@ import { render } from "react-dom";
 import "./styles.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Hello CodeSandbox</h1>
+			<h2>Start editing to see some magic happen!</h2>
+		</div>
+	);
 }
 
 const rootElement = document.getElementById("root");
---
+++ src/stores/app-store.ts
@@ -0,0 +1,3 @@
+class AppStore {}
+
+export default AppStore;
